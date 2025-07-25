74c40afd6d7cc22c222189c56a8c9101179083cd
leeway.zlw <leeway.zlw@outlook.com>
2025-07-25 18:55:39 +0800

    fix: github pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,22 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: 'export',
-  /* config options here */
-  basePath: "/home",
-};
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "";
 
+if (isGithubActions) {
+  // 去掉 `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+const nextConfig: NextConfig = {
+  output: "export",
+  /* config options here */
+  basePath,
+  assetPrefix,
+};
 
 export default nextConfig;
