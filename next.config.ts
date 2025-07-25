8298a7506bafec1ea5f70771a533b6010d19420e
leeway.zlw <leeway.zlw@outlook.com>
2025-07-25 19:02:30 +0800

    fix: github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
-const repo = process.env.GITHUB_REPOSITORY;
+const repo = process.env.GITHUB_REPOSITORY || "";
 let assetPrefix = "";
 let basePath = "";
 
